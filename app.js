807c243fd84f1e5199a589e6985fde8b7cd099f1
yishnu96 <yishnupramanik96@gmail.com>
2020-09-02 23:20:35 +0530

    added layouts

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
 
+const path = require('path');
+const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts');
+const app = express()
+const port = 5000
+
+//----------- EJS Confrigation -----------//
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use("/assets", express.static('./assets'));
+
+
+
+// --------- ROUTES -----------//
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  return res.render('signin')
+})
+
+
+app.get('/signup',(req, res)=>{
+    return res.render('signup')
+})
+
+app.get('/user',(req, res)=>{
+    return res.render('user')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Click this link to start : http://localhost:${port}`)
 })
