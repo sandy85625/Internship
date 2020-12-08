d73ed4262159bb29d48a6ac49b91eba1cbdc6d44
23ashutosh3 <1ashu7200@gmail.com>
2020-12-08 16:56:54 +0530

    useraccountauthentication

---
+++ controllers/users_controller.js
@@ -0,0 +1,42 @@
+const User = require('../models/user');
+module.exports.profile = function(req, res) {
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
+}
+module.exports.signUp = function(req, res) {
+    return res.render('user_sign_up', {
+        title: "Sign Up"
+    })
+}
+module.exports.signIn = function(req, res) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+    return res.render('user_sign_in', {
+        title: "Sign In"
+    })
+}
+module.exports.create = function(req, res) {
+    if (req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+    User.findOne({ email: req.body.email }, function(err, user) {
+        if (err) { return }
+        if (!user) {
+            User.create(req.body, function(err, user) {
+                if (err) { return }
+                return res.redirect('/users/sign-in');
+            })
+        } else {
+            return res.redirect('back');
+        }
+    });
+}
+module.exports.createSession = function(req, res) {
+    return res.redirect('/users/profile');
+}
+module.exports.destroySession = function(req, res) {
+    req.logout();
+    return res.redirect('/');
+}
